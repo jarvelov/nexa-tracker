8349637c4839748125cb2d5c9968dfd78399f893
Tobias Järvelöv <tobias.jarvelov@oderland.se>
2022-06-04 19:33:09 +0200

    wip
    Signed-off-by: Tobias Järvelöv <tobias.jarvelov@oderland.se>

--- frontend/src/api/ApiSlice.ts
+++ frontend/src/api/ApiSlice.ts
@@ -1,10 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { MeasurementsResponse, MeasurementsRequest } from './types';
 
+const protocol = process.env.NEXT_PUBLIC_API_SERVER_PROTOCOL;
+const hostname = process.env.NEXT_PUBLIC_API_SERVER_HOSTNAME;
+const port = process.env.NEXT_PUBLIC_API_SERVER_PORT;
+const path = process.env.NEXT_PUBLIC_API_SERVER_PATH;
+
+const baseUrl = `${protocol}://${hostname}:${port}${path}`;
+
+console.log('baseUrl', baseUrl);
+
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:9008/api',
+    baseUrl,
   }),
   endpoints: (builder) => ({
     getMeasurements: builder.query<MeasurementsResponse, MeasurementsRequest>({
